bc1a534acc9afc82687a93115b61545ed26cd60a
dlcksgh1 <119422058+dlcksgh1@users.noreply.github.com>
2024-04-23 23:18:52 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,7 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+
+/**
+ * 별찍기(1) - N번째 줄에 N개의 별을 찍기
+ * 1. Scanner 사용하여 입력값 받는경우
+ * 2. BufferedReader, StringBuilder 사용
+ * 3. BufferedReader, repeat(i) 반복 사용
+ */
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+
+        /*
+        1번 방법
+
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
@@ -13,5 +29,35 @@ public class Main {
             System.out.println();
         }
 
+        */
+
+        /*
+        2번 방법
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        br.close();
+        System.out.println(sb);
+        */
+        
+        //3번 방법
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+            sb.append("*".repeat(i)).append("\n");
+        }
+        br.close();
+        System.out.println(sb);
+
+        
     }
 }
