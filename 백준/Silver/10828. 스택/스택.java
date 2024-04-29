63dcecc8f9afe5e2f8850ee663ec9fbfd661a5de
dlcksgh1 <119422058+dlcksgh1@users.noreply.github.com>
2024-04-30 00:29:18 +0900

    [Silver IV] Title: 스택, Time: 364 ms, Memory: 20248 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -2,8 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
+/**
+ * ArrayList 로 스택 구현
+ */
+/*
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -41,6 +46,54 @@ public class Main {
                         System.out.println(-1);
                     } else {
                         System.out.println(stack.get(stack.size()-1));
+                    }
+                    break;
+            }
+        }
+    }
+}
+*/
+
+/**
+ * 스택 사용
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            switch (str) {
+                case "push":
+                    int input = Integer.parseInt(st.nextToken());
+                    stack.push(input);
+                    break;
+                case "pop" :
+                    if (stack.empty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.pop());
+                    }
+                    break;
+                case "size" :
+                    System.out.println(stack.size());
+                    break;
+                case "empty" :
+                    if (stack.empty()) {
+                        System.out.println(1);
+                    } else {
+                        System.out.println(0);
+                    }
+                    break;
+                case "top" :
+                    if (stack.size() == 0) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.peek());
                     }
                     break;
             }
