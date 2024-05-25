59515f01a3272e4f71667b1df39281abc59e8428
dlcksgh1 <119422058+dlcksgh1@users.noreply.github.com>
2024-05-26 00:17:12 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 124 ms, Memory: 14040 KB -BaekjoonHub

--- 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.java
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.java
@@ -2,24 +2,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         int max = 0;
+        int sum = 0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = 1; i <= 10; i++) {
             st = new StringTokenizer(br.readLine());
             int output = Integer.parseInt(st.nextToken());
             int input = Integer.parseInt(st.nextToken());
-            max += (input - output);
-            list.add(max);
+            sum += (input - output);
+            if(sum > max) max = sum;
         }
-        Collections.sort(list);
-        System.out.println(list.get(9));
+        System.out.println(max);
 
     }
 }
