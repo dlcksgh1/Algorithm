009c6a65ed531a95c12635ec0424a53d15aed329
dlcksgh1 <119422058+dlcksgh1@users.noreply.github.com>
2024-11-05 21:34:02 +0900

    [Silver III] Title: N과 M (3), Time: 440 ms, Memory: 68608 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static ArrayList<String> result;
+    static int[] str;
+    static boolean[] visited;   
+    static int cnt, n;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        cnt = Integer.parseInt(st.nextToken());
+        str = new int[cnt];
+        visited = new boolean[n];
+        dfs(0);
+        System.out.println(sb.toString());
+    }
+    
+    public static void dfs(int depth) {
+        if(depth == cnt) {
+            for(int val : str) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        for(int i = 0 ; i < n; i++) {
+            str[depth] = i+1; 
+            dfs(depth+1);
+        }
+    }
+}
