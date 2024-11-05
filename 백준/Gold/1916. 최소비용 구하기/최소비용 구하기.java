4db62b2acf8cab1346675083d0c5ef5b0ae1308e
dlcksgh1 <119422058+dlcksgh1@users.noreply.github.com>
2024-11-05 21:31:37 +0900

    [Gold V] Title: 최소비용 구하기, Time: 544 ms, Memory: 50804 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,86 +1,69 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-
-/**
- * 최단거리 구하기  다익스트라 알고리즘 사용
- */
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static int n, m;
-    static ArrayList<ArrayList<Node4>> graph = new ArrayList<ArrayList<Node4>>();
-    static int[] d;
-    static final int INF = (int) 1e9;
-    public static void main(String[] args) throws IOException {
-
+    
+    public static int[] d;
+    public static int INF = Integer.MAX_VALUE;
+    public static ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
-        d = new int[n + 1];
-        Arrays.fill(d, INF);
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<Node4>());
+        for(int i = 0 ; i <= n ; i ++) {
+            graph.add(new ArrayList<Node>());
         }
-        for (int i = 0; i < m; i++) {
+        
+        for(int i = 0 ; i < m ; i ++) {
             st = new StringTokenizer(br.readLine());
-            int index = Integer.parseInt(st.nextToken());
-            int target = Integer.parseInt(st.nextToken());
-            int distance = Integer.parseInt(st.nextToken());
-            graph.get(index).add(new Node4(target, distance));
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken()); 
+            graph.get(start).add(new Node(end, cost));
         }
         st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
+        d = new int[n+1];
+        Arrays.fill(d, INF);
         dijkstra(start);
         System.out.println(d[end]);
-
     }
-
-    static public void dijkstra(int start) {
-        PriorityQueue<Node4> pq = new PriorityQueue<>();
-        pq.offer(new Node4(start, 0));
+    
+    public static void dijkstra(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<Node>();
         d[start] = 0;
-        while (!pq.isEmpty()) {
-            Node4 node = pq.poll();
+        pq.offer(new Node(start, 0));
+        while(!pq.isEmpty()) {
+            Node node = pq.poll();
             int index = node.index;
-            int distance = node.distance;
-            if(d[index] < distance) continue;
-            for (int i = 0; i < graph.get(index).size(); i++) {
-                int cost = d[index] + graph.get(index).get(i).distance;
-                if (d[graph.get(index).get(i).index] > cost) {
-                    d[graph.get(index).get(i).index] = cost;
-                    pq.offer(new Node4(graph.get(index).get(i).index, cost));
+            int cost = node.cost;
+            if(d[index] < cost) continue;
+            for(int i = 0 ; i < graph.get(index).size(); i++) {
+                int sum = d[index] + graph.get(index).get(i).cost;
+                if(d[graph.get(index).get(i).index] > sum) {
+                    d[graph.get(index).get(i).index] = sum;
+                    pq.offer(new Node(graph.get(index).get(i).index, sum));
                 }
             }
         }
     }
-
 }
 
-class Node4 implements Comparable<Node4> {
-    public int index;
-    public int distance;
 
-    public Node4(int index, int distance) {
+class Node implements Comparable<Node> {
+    int index;
+    int cost;
+    
+    public Node(int index, int cost) {
         this.index = index;
-        this.distance = distance;
+        this.cost = cost;
     }
-
+    
     @Override
-    public int compareTo(Node4 other) {
-
-        if (this.distance > other.distance) {
-            return 1;
-        } else if (this.distance == other.distance) {
-            return 0;
-        } else {
-            return -1;
-        }
+    public int compareTo(Node other) {
+        return Integer.compare(this.cost, other.cost);
     }
 }
