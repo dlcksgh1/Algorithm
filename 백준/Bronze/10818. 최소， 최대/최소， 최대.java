2d8781728e023976fb4cf92a15c923bc8fe9ac9f
dlcksgh1 <119422058+dlcksgh1@users.noreply.github.com>
2024-11-17 17:34:25 +0900

    [Bronze III] Title: 최소, 최대, Time: 452 ms, Memory: 88028 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,19 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int cnt = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int min = 1000001, max = -1000001;
-        for (int i = 0; i < cnt; i++) {
-            int next = Integer.parseInt(st.nextToken());
-            min = (min > next) ?  next : min;
-            max = (max < next) ? next : max;
+
+        /**
+         * 정렬사용
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(list);
+        System.out.println(list.get(0) + " " + list.get(n - 1));
+        */
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if(max < num) max = num;
+            if(min > num) min = num;
         }
         System.out.println(min + " " + max);
+
     }
 }
