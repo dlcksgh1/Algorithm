535a868d65a3b280da016d2b53c728c330c9e276
dlcksgh1 <119422058+dlcksgh1@users.noreply.github.com>
2024-11-17 23:46:03 +0900

    Update 크레인 인형뽑기 게임.java

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -3,42 +3,27 @@
     그리고 그 전에 인형뽑기 할 때 위에서 아래로
 */
 import java.util.*;
-//import java.io.*;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        int answer = 0;
-        Stack<Integer> st = new Stack<>();
         
-        for(int i = 0 ; i < moves.length; i++) {
-            int move = moves[i];
-            int temp = 0;
-            for(int j = 0; j < board.length; j++) {
-                if(board[j][move-1] != 0) {
-                    temp = board[j][move-1];
-                    System.out.println("테스트" + temp);
-                    board[j][move-1] = 0;
-                    break;
-                }
-            }
-            
-            System.out.println("temp" + temp);
-            if(temp != 0) {
-                if(!st.isEmpty()){
-                    int temp2 = st.peek();
-                    System.out.println("비교1" + temp);
-                    System.out.println("비교2" + temp2);
-                    if(temp2 == temp) {
-                        st.pop();
-                        answer += 2;
-                    } else {
-                        st.push(temp);
-                    }
-                } else {
-                    st.push(temp);
-                }
+        int answer = 0;
+        Stack <Integer> stack = new Stack<>();
+        stack.push(0);
 
+        for (int move : moves) {
+          for (int j = 0; j < board.length; j++) {
+            if (board[j][move - 1] != 0) {
+              if (stack.peek() == board[j][move - 1]) {
+                stack.pop();
+                answer += 2;
+              } else {
+               stack.push(board[j][move - 1]);
+              }
+              board[j][move - 1] = 0;
+              break;
             }
+          }
         }
         return answer;
     }
