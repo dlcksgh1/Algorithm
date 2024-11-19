1d11da08b830c0d9abfb2b5b66e2dc5b742710e5
dlcksgh1 <119422058+dlcksgh1@users.noreply.github.com>
2024-11-19 23:28:57 +0900

    [Silver II] Title: 부분수열의 합, Time: 128 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,42 +1,41 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
- 
+
+/**
+ * 조합문제 부분 수열을 전부 구하자
+ */
 public class Main {
- 
-	static int N, S;
-	static int arr[], result[];
-	static int cnt;
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		S = Integer.parseInt(st.nextToken());
-		
-		arr = new int[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		dfs(0, 0);
-		if(S == 0) {
-			System.out.println(cnt-1);
-		}else {
-			System.out.println(cnt);
-		}
-	}
-	
-	private static void dfs(int index, int sum) {
-		if(index == N) {
-			if(sum == S) {
-				cnt++;
-			}
-			return;
-		}
-		dfs(index+1, sum+arr[index]);
-		dfs(index+1, sum);
-	}
+
+    static int result = 0;
+    static ArrayList<Integer> list;
+    static int n, target;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        target = Integer.parseInt(st.nextToken());
+        list = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        dfs(0, 0); // 탐색 시작
+        if (target == 0) result--; // 공집합 제외
+        System.out.println(result);
+
+    }
+    static void dfs(int depth, int sum) {
+
+        if(depth == n) {
+            if(sum == target) result++;
+            return;
+        }
+
+        dfs(depth + 1, sum + list.get(depth)); // 포함
+        dfs(depth + 1, sum);  // 미포함
+    }
 }
