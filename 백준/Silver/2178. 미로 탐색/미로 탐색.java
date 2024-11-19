6c31217e5acc8a11d0164ddd39b491627eb4b5a2
dlcksgh1 <119422058+dlcksgh1@users.noreply.github.com>
2024-11-19 14:43:52 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14688 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,83 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
- * 미로찾기 (BFS) 사용하여 구현
- * BFS 를 사용하여 넓이우선탐색을 진행하여 1여부 판단 후 이전 node 에서 cnt++
+ * bfs로 풀거임
+ * 해당 문제는 visited 여부 필요 없이 1 인 곳을 갈건데 방문하면
+ * 그 해당 칸에 몇번째로 이동했는지에 대한 값을 넣을거임
  */
-
 public class Main {
+
+    static int[][] map;
+    static int n, m;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int x = Integer.parseInt(st.nextToken());
-        int y = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[x][y];
-        int[][] visited = new int[x][y];
-
-        // 배열 초기화
-        for (int i = 0; i < x; i++) {
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        for (int i = 0; i < n; i++) {
             String str = br.readLine();
-            for (int j = 0; j < y; j++) {
-                arr[i][j] = str.charAt(j) - '0';
+            for (int j = 0; j < m; j++) {
+                map[i][j] = str.charAt(j) - '0';
             }
         }
+        bfs();
+
+        System.out.println(map[n-1][m-1]);
 
 
-
-        int result = bfs(arr, visited);
-        System.out.println(result);
     }
 
-    private static int bfs(int[][] arr, int[][] visited) {
-        Queue<Node3> queue = new LinkedList<>();
-        queue.offer(new Node3(0, 0));
-        visited[0][0] = 1;
-
+    public static void bfs(){
         int[] dx = {0, 0, -1, 1};
         int[] dy = {-1, 1, 0, 0};
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(0, 0));
 
-        while (!queue.isEmpty()) {
-            Node3 node = queue.poll();
-            int x = node.getX();
-            int y = node.getY();
+        while(!queue.isEmpty()) {
+            Node node = queue.poll();
             for (int i = 0; i < 4; i++) {
-                int newX = x + dx[i];
-                int newY = y + dy[i];
+                int nx = node.x + dx[i];
+                int ny = node.y + dy[i];
+                if(nx < 0 || nx >=n || ny < 0 || ny >= m) continue;
 
-                if (0 <= newX && newX < arr.length && 0 <= newY && newY < arr[0].length) {
-                    if (visited[newX][newY] == 0 && arr[newX][newY] == 1) {
-                        queue.offer(new Node3(newX, newY));
-                        visited[newX][newY] = visited[x][y] + 1;
-                    }
+                if(map[nx][ny] == 1 ){
+                    map[nx][ny] = map[node.x][node.y] + 1;
+                    queue.offer(new Node(nx, ny));
                 }
             }
         }
 
-        return visited[arr.length - 1][arr[0].length - 1];
     }
-
 }
 
-class Node3 {
-    private int x;
-    private int y;
-
-    public Node3(int x, int y) {
+class Node {
+    int x;
+    int y;
+    public Node(int x, int y) {
         this.x = x;
         this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
     }
 }
