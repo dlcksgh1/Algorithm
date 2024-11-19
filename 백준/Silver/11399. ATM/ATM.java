6cf1db37789cb4d4d6a0ce21097c5380e8f73ce6
dlcksgh1 <119422058+dlcksgh1@users.noreply.github.com>
2024-11-19 13:17:21 +0900

    [Silver IV] Title: ATM, Time: 136 ms, Memory: 14524 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,23 +1,26 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
-
-public class Main{
-	public static void main(String args[])throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		ArrayList<Integer>list = new ArrayList<>();
-		for(int i = 0 ; i < n ; i++){
-			list.add(Integer.parseInt(st.nextToken()));
-		}
-		Collections.sort(list);
-		int sum = 0;
-		int wait = 0;
-		for(int time : list){
-			wait += time;
-			sum += wait;
-		}
-		System.out.println(sum);
-	}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(list);
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                sum += list.get(j);
+            }
+        }
+        System.out.println(sum);
+    }
 }
