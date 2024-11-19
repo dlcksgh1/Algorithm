57219dc1a962e46a2e545364faf06a3bb5ffaf4b
dlcksgh1 <119422058+dlcksgh1@users.noreply.github.com>
2024-11-19 13:43:05 +0900

    [Silver I] Title: 회의실 배정, Time: 484 ms, Memory: 44580 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,55 +1,50 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
-class Meeting implements Comparable<Meeting>{
-	private int st;
-	private int end;
-	public void setSt(int st){
-		this.st = st;
-	}
-	public int getSt(){
-		return this.st;
-	}
-	public void setEnd(int end){
-		this.end = end;
-	}
-	public int getEnd(){
-		return this.end;
-	}	
-	public Meeting(int st, int end){
-		this.st = st;
-		this.end = end;
-	}
-	
-	@Override
-	public int compareTo(Meeting other){
-		if(this.end == other.end) {
-			return this.st >= other.st ? 1 : -1;
-		}
-		return this.end >= other.end ? 1 : -1;
-	}
-}
+/**
+ * 회의실 배정
+ * 시작 끝나는 시간이있고 회의실은 한개임
+ * 이게 최대로 사용할 수 있는 개수를 찾아야함
+ * 시작시간 종료시간을 입력받고 종료시간을
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
 
-public class Main{
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int n = Integer.parseInt(br.readLine());
-		ArrayList<Meeting>list = new ArrayList<>();
-		for(int i = 0 ; i < n ; i++){
-			st = new StringTokenizer(br.readLine()); 
-			list.add(new Meeting(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-		}
-		Collections.sort(list);
-		int count = 1;
-		int end = list.get(0).getEnd();
-		for(int i = 1; i < n ; i++){
-			int start = list.get(i).getSt();
-			if(start >= end){
-				count++;
-				end = list.get(i).getEnd();
-			}
-		}
-		System.out.println(count);
-	}
+        int[][] meetings = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            meetings[i][0] = Integer.parseInt(st.nextToken());
+            meetings[i][1] = Integer.parseInt(st.nextToken());
+
+        }
+        Arrays.sort(meetings, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1] == o2[1]) {
+                    return o1[0] -o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+        });
+
+        int count = 0;
+        int lastEndTime = 0;
+
+        // 회의 배정
+        for (int[] meeting : meetings) {
+            if (meeting[0] >= lastEndTime) {
+                count++;
+                lastEndTime = meeting[1];
+            }
+        }
+
+        System.out.println(count);
+    }
 }
