c72b46f46b464b07dbb58d626a18537c31526033
dlcksgh1 <119422058+dlcksgh1@users.noreply.github.com>
2024-12-11 17:30:35 +0900

    [level 2] Title: 프로세스, Time: 3.21 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,63 +1,36 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
-        Queue<Process> queue = new LinkedList<>();
-        for (int i = 0; i < priorities.length; i++) {
-            queue.offer(new Process(i , priorities[i]));
+        
+        Queue<int[]> queue = new LinkedList<>();
+        for(int i = 0 ; i < priorities.length; i++) {
+            queue.offer(new int[]{i, priorities[i]});
         }
-        Integer[] newPriorities = Arrays.stream(priorities).boxed().toArray(Integer[]::new);
-        Arrays.sort(newPriorities, Collections.reverseOrder());
-
-        boolean match = false;
-        for (int i = 0; i < newPriorities.length; i++) {
-
-            while(true) {
-                if (queue.peek().priority == newPriorities[i]) {
-                    Process compareProcess = queue.poll();
-                    if (compareProcess.location == location) {
-                        match =true;
-                    }
+        
+        int printOrder = 0;
+        
+        while(!queue.isEmpty()) {
+            int[] current = queue.poll();
+            boolean hasHigher = false;
+            
+            for(int[] doc : queue) {
+                if(doc[1] > current[1]) {
+                    hasHigher = true;
                     break;
-                } else {
-                    queue.offer(queue.poll());
                 }
             }
-            if (match) {
-                answer = i + 1;
-                break;
+            
+            if(hasHigher) {
+                queue.offer(current);
+            } else {
+                printOrder++;
+                if(current[0] == location) {
+                    return printOrder;
+                }
             }
         }
-        return answer;
-    }
-}
-
-class Process {
-    int location;
-    int priority;
-
-    public Process(int location, int priority) {
-        this.location = location;
-        this.priority = priority;
-    }
-
-    public int getLocation() {
-        return location;
-    }
-
-    public void setLocation(int location) {
-        this.location = location;
-    }
-
-    public int getPriority() {
-        return priority;
-    }
-
-    public void setPriority(int priority) {
-        this.priority = priority;
+        
+        return -1;
     }
 }
