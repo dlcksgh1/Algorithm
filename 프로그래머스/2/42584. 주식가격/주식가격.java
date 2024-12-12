3e20cb549117aafd6169022876a9706c44cff25a
dlcksgh1 <119422058+dlcksgh1@users.noreply.github.com>
2024-12-13 01:15:27 +0900

    [level 2] Title: 주식가격, Time: 24.69 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,19 +3,14 @@ class Solution {
         int[] answer = new int[prices.length];
 
         for (int i = 0; i < prices.length; i++) {
-            int price = prices[i];
-            int cnt = 0;
             for (int j = i + 1; j < prices.length; j++) {
-                if (price <= prices[j]) {
-                    cnt++;
-                } else {
-                    cnt++;
+                answer[i]++;
+                if(prices[i] > prices[j]) {
                     break;
                 }
             }
-            answer[i] = cnt;
         }
-        answer[prices.length - 1] = 0;
+
         return answer;
     }
 }
